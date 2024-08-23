72327a09868791be1a84a5be00ac0bbc8cf23c2e
Felipe <166918384+vlipe@users.noreply.github.com>
2024-08-23 10:26:35 -0300

    08/05/2024

---
+++ TPA/Segundo Bimestre/Pj_0805/src/ExemploUm.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class ExemploUm {
+    public static void main(String[] args) {
+        System.out.print("Olá!");
+    }
+}
