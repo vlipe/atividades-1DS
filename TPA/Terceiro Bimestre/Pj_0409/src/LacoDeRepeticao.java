64538dfab22dccc5fd37dc644a912b9201d1916a
Felipe <166918384+vlipe@users.noreply.github.com>
2024-09-15 17:25:09 -0300

    04/09/2024

---
+++ TPA/Terceiro Bimestre/Pj_0409/src/LacoDeRepeticao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author apare
+ */
+public class LacoDeRepeticao {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 200; i++){
+            if (i %2 == 0)
+            System.out.println("Número: " + i);
+        }
+    }
+}
