007487c1c37aca24e63af3aed78d6654f0aadc4e
Felipe <166918384+vlipe@users.noreply.github.com>
2025-02-25 21:02:02 -0300

    D.S.

---
+++ Técnicas da Programação e Algoritmos/2° Bimestre/Pj_0805/src/ExemploDois.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class ExemploDois {
+    public static void main(String[] args) {
+        int soma=0;
+        int num1=10;
+        int num2=7;
+        soma=num1+num2;
+        System.out.print(num1+" + "+num2+" = "+soma);
+    }
+   
+}
